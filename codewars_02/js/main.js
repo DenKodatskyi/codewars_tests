50c0344f281a95542fcd4cb208da4548d0371478
Den Kodatskyi <denkodak@gmail.com>
2024-05-30 15:34:48 +0300

    Add files via upload

--- codewars_02/js/main.js
+++ codewars_02/js/main.js
@@ -7,15 +7,26 @@ You can not use multiplier "*" operator.
 
 If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).
 */
-
-function billboard(name, price = 30) {
-
-  var totalCost = 0;
-  for (i = 0; i < name.length; i++) {
-    totalCost += price;
+/* ========================================================================================================================== */
+// #1
+const billboard = (name, price = 30) => {
+  let result = 0
+  for (let i = 0; i < name.length; i++) {
+    result += price
   }
-
-  return totalCost;
-
+  return result
 }
+
+// #2
+// const billboard = (name, price = 30) => name.length * price;
+
+// #3
+// function billboard(name, price = 30) {
+//   var totalCost = 0;
+//   for (i = 0; i < name.length; i++) {
+//     totalCost += price;
+//   }
+
+//   return totalCost;
+// }
 
