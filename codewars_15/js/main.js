ff11c9d176b9a224980cf84d17094602b26737e8
Den Kodatskyi <denkodak@gmail.com>
2024-06-05 12:54:11 +0300

    Add files via upload

---
+++ codewars_15/js/main.js
@@ -0,0 +1,12 @@
+/*
+8 kyu
+Type of sum
+
+Return the type of the sum of the two arguments
+*/
+// ====================================================================================================================
+const typeOfSum = (a, b) => typeof (a + b);
+
+
+
+
