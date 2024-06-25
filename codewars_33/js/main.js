100204c6513eb3aa49e06433041c77af5adf8ea7
Den Kodatskyi <denkodak@gmail.com>
2024-06-25 08:50:10 +0300

    Add files via upload

---
+++ codewars_33/js/main.js
@@ -0,0 +1,27 @@
+/*
+8 kyu
+Convert a Boolean to a String
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+/* ==================================================================================================================== */
+
+// #1
+const booleanToString = (b) => b.toString();
+
+// #2
+// const booleanToString = (b) => b + '';
+
+// #3
+// const booleanToString = (b) => {
+//   return b.toString()
+// }
+
+
+
+
+
+
