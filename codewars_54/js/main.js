803dd3eb2f104d23e9dc5e8bc76d06bc2a6f7021
Den Kodatskyi <denkodak@gmail.com>
2024-07-31 09:43:29 +0300

    Add files via upload

---
+++ codewars_54/js/main.js
@@ -0,0 +1,17 @@
+/*
+8 kyu
+MakeUpperCase
+
+Write a function which converts the input string to uppercase.
+
+*/
+/* ==================================================================================================================== */
+
+const makeUpperCase = (str) => {
+  return str.toUpperCase();
+};
+
+
+
+
+
