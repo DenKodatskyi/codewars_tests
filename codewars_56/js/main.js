6147d9dc431b512fdbb74fd8c7014c2de3cfd99d
Den Kodatskyi <denkodak@gmail.com>
2024-08-02 10:27:42 +0300

    Add files via upload

---
+++ codewars_56/js/main.js
@@ -0,0 +1,36 @@
+/*
+8 kyu
+Beginner Series #1 School Paperwork
+
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+Example:
+n= 5, m=5: 25
+n=-5, m=5:  0
+Waiting for translations and Feedback! Thanks!
+
+*/
+/* ==================================================================================================================== */
+
+// #1
+const paperwork = (n, m) => (n < 0 || m < 0) ? 0 : n * m;
+
+// #2
+// const paperwork = (n, m) => {
+//   return (n < 0 || m < 0) ? 0 : n * m;
+// };
+
+// #3
+// const paperwork = (n, m) => {
+//   if(n < 0 || m < 0) {
+//     return 0
+//   }
+//   return n * m
+// };
+
+
+
+
+
