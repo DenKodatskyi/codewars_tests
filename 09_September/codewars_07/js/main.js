bd0a12924fad67f96168d58113b480c8c17c01ba
Den Kodatskyi <denkodak@gmail.com>
2024-09-08 15:00:20 +0300

    Add files via upload

---
+++ 09_September/codewars_07/js/main.js
@@ -0,0 +1,23 @@
+/*
+8 kyu
+Third Angle of a Triangle 
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+
+*/
+/* ==================================================================================================================== */
+
+const otherAngle = (a, b) => {
+  return 180 - (a + b);
+};
+
+
+
+
+
